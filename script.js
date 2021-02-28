e22f20751e40c6e849e9b0a52b1af976bec0a76b
Fatlind Shehu <fatlind408@gmail.com>
2021-02-27 23:13:59 -0500

    Initiating logic

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 'use strict';
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No guess';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Success!!!';
+    score++;
+    document.querySelector('.score').textContent = score;
+  } else if (guess > secretNumber) {
+    if (score > 0) {
+      document.querySelector('.message').textContent = 'To big';
+      console.log(score);
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game!';
+    }
+  } else if (score < secretNumber) {
+    if (score > 0) {
+      document.querySelector('.message').textContent = 'To small';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game!';
+    }
+  }
+});
