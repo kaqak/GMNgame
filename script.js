66c9ebb92ecbc326295685f266c2098087e9d76d
Fatlind Shehu <fatlind408@gmail.com>
2021-02-28 20:37:41 -0500

    Fixing a bug related with score zero

--- script.js
+++ script.js
@@ -1,47 +1,52 @@
-'use strict';
+'use strict ';
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
 let score = 20;
 
 document.querySelector('.check').addEventListener('click', function () {
-  let guess = Number(document.querySelector('.guess').value);
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
   //When there's no input
   if (!guess) {
-    document.querySelector('.message').textContent = 'No guess';
-
-    //When player wins
+    document.querySelector('.message').textContent = `No number!`;
+    //When the player wins
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = 'Success!!!';
-    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector(
+      '.message'
+    ).textContent = `You guessed the right number!`;
     score++;
-    document.querySelector('.score').textContent = score;
-
-    document.body.style.backgroundColor = 'Green';
+    document.body.style.backgroundColor = 'green';
     document.querySelector('.number').style.width = '30rem';
+    //When input is to high
   } else if (guess > secretNumber) {
-    if (score > 0) {
-      document.querySelector('.message').textContent = 'To big';
-      console.log(score);
+    if (score > 1) {
+      document.querySelector('.message').textContent = `To big!!!`;
       score--;
       document.querySelector('.score').textContent = score;
     } else {
-      document.querySelector('.message').textContent = 'You lost the game!';
+      document.querySelector('.message').textContent = `You lost the game!!!`;
+      document.querySelector('.score').textContent = 0;
     }
-    //When guess is to low
-  } else if (score < secretNumber) {
-    if (score > 0) {
-      document.querySelector('.message').textContent = 'To small';
+    //When input is to low
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = `To small!!!`;
       score--;
       document.querySelector('.score').textContent = score;
     } else {
-      document.querySelector('.message').textContent = 'You lost the game!';
+      document.querySelector('.message').textContent = `You lost the game!!!`;
+      document.querySelector('.score').textContent = 0;
     }
   }
 });
 
 document.querySelector('.again').addEventListener('click', function () {
+  document.body.style.backgroundColor = '';
+  document.querySelector('.message').textContent = `Guess the number!!!`;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.number').textContent = secretNumber;
+  document.querySelector('.guess').value = '';
   score = 20;
-  const secretNumber = Math.trunc(Math.random() * 20) + 1;
-  document.querySelector('.message').textContent = 'Start guessing...';
-  
+  document.querySelector('.score').textContent = score;
 });
